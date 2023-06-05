7351412dcaf0c23e7496aed05cfc9226fb6bc2a5
bloodyhand23 <bloody_hand@icloud.com>
2023-06-05 21:54:08 +0500

    123

---
+++ 2023/java script/java script/js/script.js
@@ -0,0 +1,122 @@
+"use strict"
+//console.log("Я учу JS!");
+
+/*
+let userColorEyes = "black";
+console.log(userColorEyes);
+*/
+
+
+/*
+let user;
+let userName = "Вася";
+
+user = userName;
+console.log(user);
+*/
+
+/*
+let userAge = 36;
+let userInfo = "Фрилансер ${userAge}";
+console.log(userInfo);
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+let userName;
+console.log(typeof userName);
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
+*/
+
+
+/*
+console.log('35' + - "22");
+
+console.log('35' * "22");
+
+//console.log('558' > 22++);
+
+let uC = 0;
+let nU = uC++;
+console.log(nU);
+
+console.log(!false && 11 || 18 && !'');
+
+let oper = 0;
+console.log(oper ?? "no name");
+*/
+
+/*
+if (1 === "1") {
+	console.log('Истина!');
+} else {
+	console.log('Ложь!');
+}
+
+if (5 == "5") {
+	console.log('Истина!');
+} else {
+	console.log('Ложь!');
+}
+
+let message = (92 > '11' && 58 < 100) ? 'Истина!' : 'Ложь!';
+console.log(message);
+
+if (0) {
+	console.log('Ложь!');
+} else if (" ") {
+	console.log('Истина!');
+}
+*/
+
+/*
+let a=1;
+while (a<6){
+	console.log(a);
+	a++;
+}
+*/
+
+/*
+let a=1;
+do {
+	console.log(a);
+	a++;
+} while (a<6);
+*/
+
+/*
+let a;
+for(a=1;a<6;a++){
+	console.log(a);
+}
+*/
+
+/*
+let a=8;
+while (a){
+	console.log(a);
+	a--;
+}
+*/
+
+/*
+let num=0;
+while(num<3){
+	console.log(`Число: ${num}`);
+	num++;
+}
+*/
+
+/*
+firstFor: for (let num=0; num<2; num++){
+	for (let size=0; size<3; size++){
+	console.log(size);
+		if (size==1) {
+			break firstFor;
+		}
+	}
+}
+*/
